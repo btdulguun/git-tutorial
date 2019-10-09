72eeda89fddac370705f1696cf0c4270109fe1d7
btdulguun <munhjargaltsedev@yahoo.com>
2019-10-09 21:04:15 +0900

    last commit

--- kadai2.rb
+++ kadai2.rb
@@ -1,12 +1,15 @@
 for i in 1..100
 amari3 = i % 3
 amari5 = i % 5
+amari7 = i % 7
 if amari3 == 0 && amari5 == 0 then
 print "FizzBuzz\n"
 elsif amari3 == 0 then
 print "Fizz\n"
 elsif amari5 == 0 then
 print "Buzz\n"
+elsif amari7 == 0 then
+print "git\n"
 else
 print "#{i} \n" 
 end
